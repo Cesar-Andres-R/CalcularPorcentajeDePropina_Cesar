c8b101d129ae2ab81bbc804852daa99e8ca5d27c
Perla118 <perlamaria9545@gmail.com>
2023-11-23 15:02:41 -0700

    Agregar archivos de proyecto.

---
+++ CalcularPorcentajeDePropina_Cesar/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalcularPorcentajeDePropina_Cesar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Calcular(object sender, EventArgs e)
+        {
+            //variables
+            var precioPagar = float.Parse(monto.Text);
+            var porcentajeXd = float.Parse(porcentaje.Text);
+            var compartenCuentaXd = float.Parse(compartenCuenta.Text);
+
+            //respuesta
+            var respuesta = precioPagar / compartenCuentaXd;
+
+
+            
+
+            Respuesta.Text = respuesta.ToString();
+
+            string mensaje = " ";
+
+            if (porcentajeXd < 10)
+            {
+
+                mensaje = "Eres bien codo" + respuesta;
+                Triste.IsVisible = true;
+                Feliz.IsVisible = false;
+            }
+            else if (porcentajeXd >= 10)
+            {
+                mensaje = "No eres codo" + respuesta;
+                Triste.IsVisible = false;
+                Feliz.IsVisible = true;
+            }
+            DisplayAlert("Respuesta", mensaje, "Quitar");
+        }
+
+        
+
+    }
+
+
+}
